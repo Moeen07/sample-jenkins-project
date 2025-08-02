d8e3c359905538896b575ca864dcdd051e9ab5d4
Moeen07 <moeenmoonis57@gmail.com>
2025-08-02 17:30:26 +0500

    MUST WORK THIS TIME NOW AGAIN

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/health")
 async def health_check():
-    return JSONResponse(content={"message": "Service is running, and this time it will build with Jenkins"}, status_code=200)
+    return JSONResponse(content={"message": "Service is running, and MUST WORK THIS TIME"}, status_code=200)
